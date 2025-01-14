cbb33fe74af5629253304fd4dbc83a4da584b9b8
Ardhi Lukianto <ardhi@lukianto.com>
2025-01-14 19:06:36 +0700

    Updates

--- dobo/method/record/remove.js
+++ dobo/method/record/remove.js
@@ -9,14 +9,15 @@ async function recordRemove ({ schema, id, options = {} }) {
 
   const mod = await importModule(`${this.name}:/dobo/lib/${driver.type}/record-remove.js`)
   const errorHandler = await importModule(`${this.name}:/dobo/lib/${driver.type}/error-handler.js`)
+  let old
+  if (!noResult) old = await getRecord.call(this, { schema, id })
   try {
     if (mod) await mod.call(this, { schema, id, options })
     else await instance.client(schema.name).where('id', id).del()
   } catch (err) {
     throw errorHandler ? (await errorHandler.call(this, err)) : (await defErrorHandler.call(this, err))
   }
-  const rec = noResult ? undefined : await getRecord.call(this, { schema, id })
-  return noResult ? undefined : { oldData: rec.data }
+  return noResult ? undefined : { oldData: old.data }
 }
 
 export default recordRemove
