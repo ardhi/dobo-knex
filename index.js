b2cd0078ed305eabd67163072ff0b9605bd256f9
Ardhi Lukianto <ardhi@lukianto.com>
2025-09-28 20:58:52 +0700

    Updates

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
+/**
+ * Plugin factory
+ *
+ * @param {string} pkgName - NPM package name
+ * @returns {class}
+ */
 async function factory (pkgName) {
   const me = this
 
+  /**
+   * DoboKnex class
+   *
+   * @class
+   */
   class DoboKnex extends this.app.pluginClass.base {
     static alias = 'dbknex'
     static dependencies = ['dobo']
